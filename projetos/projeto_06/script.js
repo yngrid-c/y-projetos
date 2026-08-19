6ada7d54ef8cafec5b05b960319ee005461a021c
YNGRID CRISTINA SIQUEIRA DA SILVA <yngrid_c_silva@estudante.sesisenai.org.br>
2026-08-19 16:23:35 -0300

    .

--- projetos/projeto_06/script.js
+++ projetos/projeto_06/script.js
@@ -22,6 +22,27 @@ function adicionarTarefa() {
         contadorId++
         tarefas.push({id: contadorId, tarefas: texto, concluido: false})
         inputText.value = ""
-        console.log(tarefas)
+        renderizar()
     }
+}
+
+function renderizar() {
+    lista.innerHTML = ""
+    tarefas.forEach(function(item){
+        let li = document.createElement("li")
+        li.textContent = item.tarefas
+        lista.appendChild(li)
+
+        let btnExcluir = document.createElement ("button")
+        btnExcluir.innerText = "❌"
+        li.appendChild(btnExcluir)
+        btnExcluir.addEventListener("click", function(e){
+            e.stopPropagation()
+
+            tarefas = tarefas.filter (function(t){
+                return t.id !== item.id
+            })
+            renderizar()
+        })
+    })
 }
