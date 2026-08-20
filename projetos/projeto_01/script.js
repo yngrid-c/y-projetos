14d5901426ea3f0186c82e8fb313de42cf244c20
yngrid-c <yngrid_c_silva@estudante.sesisenai.org.br>
2026-08-19 23:36:25 -0300

    .

--- projetos/projeto_01/script.js
+++ projetos/projeto_01/script.js
@@ -1,12 +1,27 @@
+
+// equaçao (ele cria a variavel com toda a equaçao dentro), quando chama a variavel mostra a resposta final
 function criarTabuleiro(){
-    const tabuleiro = document.querySelector(".tabuleiro");
 
-    for (let c = 1; c <= 8; c++) {
-        for (let i = 1; i <= 8; i++) {
-    let casa = document.createElement("div");
-    casa.classList.add("casa");
-    tabuleiro.appendChild(casa);
+  //ponte ente html e js
+  const tabuleiro = document.querySelector(".tabuleiro");
 
+  //cria as fileira (as 6 tentativas)
+  for (let c = 1; c <= 8; c++) {
+
+    //cria as caixinhas (as 5 letras)
+    for (let i = 1; i <= 8; i++) {
+
+      //cE cria uma tag no html
+      let casa = document.createElement("div");
+
+      //cL interliga a tag (div = variavel casa) ao css, o add veste a roupa (permite q o usuario veja,as bordas/cores, etc)
+      casa.classList.add("casa");
+
+      //pega os quadradinhos feito coloca dentro das fileiras
+      tabuleiro.appendChild(casa);
+      
+
+      //soma para os pares ficar preto
       if((c + i) % 2 == 0) {
         casa.classList.add("preto");
       }
--- projetos/projeto_02/script.js
+++ projetos/projeto_02/script.js
@@ -1,11 +1,19 @@
 const modal = document.querySelector(".modal");
 const btnAbrir = document.querySelector(".btnAbrir");
-const btnFechar = document.querySelector("btnFechar");
+const btnFechar = document.querySelector(".btnFechar");
 
+
+//quando o usuario clicar vai mostrar o conteudo do function
 btnAbrir.addEventListener("click", function(){
+
+    //no css o modal ta em none( eixa tudo invisivel) mas quando aperta o isactive permite ver
     modal.classList.add("is-active");
 });
 
+
+//quando clicar vai fechar
 btnFechar.addEventListener("click", function(){
+
+    //vai remover oq foi oermitido eu ver
     modal.classList.remove("is-active");
 });
